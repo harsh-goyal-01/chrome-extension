4d3e723c74da2dfaf8bbf8a97954eec611d5f008
naman1608 <1608naman@gmail.com>
2023-12-10 23:00:11 +0530

    updates

--- popup.js
+++ popup.js
@@ -1,11 +1,44 @@
 document.addEventListener('DOMContentLoaded', function () {
     let extractButton = document.getElementById('extractButton');
+
+    let upvoted = -1;
     
+    const handleVote = (action) => {
+      console.log('clicked');
+      const thumbsUp = document.querySelector('.thumbs-up');
+      const thumbsDown = document.querySelector('.thumbs-down');
+
+      if (action === 'up') {
+        thumbsUp.classList.add('clicked');
+        thumbsDown.classList.remove('clicked');
+        console.log('User voted thumbs up');
+        upvoted = 1;
+      } else {
+        thumbsDown.classList.add('clicked');
+        thumbsUp.classList.remove('clicked');
+        console.log('User voted thumbs down');
+        upvoted = 0;
+      }
+    }
+
     extractButton.addEventListener('click', function () {
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         let activeTab = tabs[0];
         chrome.tabs.sendMessage(activeTab.id, { action: 'extractSubreddit' });
       });
     });
+
+    let upButton = document.getElementById("up-button");
+
+    upButton.addEventListener("click", function () {
+      handleVote("up");
+    });
+
+    let downButton = document.getElementById("down-button");
+
+    downButton.addEventListener("click", function () {
+      handleVote("down");
+    });
+
   });
   
